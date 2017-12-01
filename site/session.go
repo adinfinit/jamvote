b5d64d763760da0c804829c966833f482947af48
Egon Elbre <egonelbre@gmail.com>
2017-12-01 15:19:49 +0200

    Add cookiestore env variable.

--- site/session.go
+++ site/session.go
@@ -1,6 +1,10 @@
 package site
 
 import (
+	"crypto/rand"
+	"log"
+	"os"
+
 	"github.com/gorilla/sessions"
 )
 
@@ -9,7 +13,18 @@ const DefaultSessionName = "jamvote"
 var SessionStore sessions.Store
 
 func init() {
-	cookieStore := sessions.NewCookieStore([]byte("TODO TODO TODO TODO TODO"))
+	secret := []byte(os.Getenv("COOKIESTORE_SECRET"))
+	if len(secret) == 0 {
+		log.Println("Cookie Secret missing")
+		var code [64]byte
+		_, err := rand.Read(code[:])
+		if err != nil {
+			log.Println(err)
+			secret = code[:]
+		}
+	}
+
+	cookieStore := sessions.NewCookieStore(secret)
 	cookieStore.Options = &sessions.Options{
 		Path:     "/",
 		HttpOnly: true,
