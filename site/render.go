b5cf93c1042f076ed0cd988a11b8fb06379b2396
Egon Elbre <egonelbre@gmail.com>
2017-12-03 12:21:44 +0200

    Include game name in team page header.

--- site/render.go
+++ site/render.go
@@ -34,16 +34,23 @@ func (server *Server) initTemplates(glob string) error {
 		"violinLeft":  violinLeft,
 		"violinRight": violinRight,
 
-		"averageViolinScore": func(min, max float64, xs []float64) float64 {
-			if len(xs) == 0 {
-				return 50
+		"averageViolinScore": func(min, max float64, xs interface{}) float64 {
+			if x, ok := xs.(float64); ok {
+				return 100 - 100*(x-min)/(max-min)
 			}
-			t := 0.0
-			for _, x := range xs {
-				t += x
+			if xs, ok := xs.([]float64); ok {
+				if len(xs) == 0 {
+					return 50
+				}
+				t := 0.0
+				for _, x := range xs {
+					t += x
+				}
+				a := t / float64(len(xs))
+				return 100 - 100*(a-min)/(max-min)
 			}
-			a := t / float64(len(xs))
-			return 100 - 100*(a-min)/(max-min)
+
+			return -100
 		},
 
 		"add": func(a, b interface{}) float64 {
@@ -73,7 +80,7 @@ func (context *Context) Render(name string) {
 	}
 }
 
-const violinStep = 0.1
+const violinStep = 0.05
 
 func violin(min, max float64, scores []float64) []float64 {
 	var values []float64
